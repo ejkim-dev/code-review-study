119c0e3e0ce956dd3d96fa71c7ee8d960fda1e88
HoonKim-1million <kimhoon@1milliondance.com>
2022-04-04 16:46:53 +0900

    add : Parcelize 플러그인 추가

--- app/src/main/java/com/example/baseandroidapp/core/presentation/VodView.kt
+++ app/src/main/java/com/example/baseandroidapp/core/presentation/VodView.kt
@@ -1,10 +1,15 @@
 package com.example.baseandroidapp.core.presentation
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class VodView(
     val id: Int,
     val title: String,
     val imageUrl: String,
-)
+) : Parcelable
+
 
 data class VodDetailView(
     val id: Int,
