0aa58a2f5da31b4fbd72c693846a7908e84fc031
HoonKim-1million <kimhoon@1milliondance.com>
2022-04-14 09:55:37 +0900

    fix : datasource.factory에 userAgent 추가
    - exoplayer 버전 변경에 따라 DefaultHttpDataSourceFactory deprecated
    - DefaultHttpDataSource.Factory()로 이전하면서 userAgent가 필요없는 줄 알았지만 nullable하게 변경되었길래 setUserAgent()를 사용하여 추가함

--- app/src/main/java/com/example/baseandroidapp/sample/exoplayer/ExoplayerActivity.kt
+++ app/src/main/java/com/example/baseandroidapp/sample/exoplayer/ExoplayerActivity.kt
@@ -14,68 +14,65 @@ import com.google.android.exoplayer2.upstream.cache.SimpleCache
 
 class ExoplayerActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityExoplayerBinding
+    private lateinit var binding: ActivityExoplayerBinding
 
     private lateinit var player: ExoPlayer
 
     /** Player UI View */
     private lateinit var playerView: StyledPlayerView
 
+    private val simpleCache: SimpleCache = AndroidApplication.simpleCache!!
+
     companion object {
         fun callingIntent(context: Context) = Intent(context, ExoplayerActivity::class.java)
     }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityExoplayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         val streamUrl =
             "https://one-m-online.s3.ap-northeast-2.amazonaws.com/online-lecture/1638332287717/HLS/1638332287717_TRAILER.m3u8"
-        val url = "https://1milliondanceplus.s3.us-west-1.amazonaws.com/tutorial/10/Tina+Boo-B-Quick+Moves-1-Trailer.mp4"
-
+        val url =
+            "https://1milliondanceplus.s3.us-west-1.amazonaws.com/tutorial/10/Tina+Boo-B-Quick+Moves-1-Trailer.mp4"
 
         binding.btnMp4.setOnClickListener {
             Toast.makeText(this, "play mp4", Toast.LENGTH_SHORT).show()
-            if (::player.isInitialized) player.release()
+            releasePlayer()
             initPlayer(url = url)
 
         }
         binding.btnStream.setOnClickListener {
             Toast.makeText(this, "play stream", Toast.LENGTH_SHORT).show()
-            if (::player.isInitialized) player.release()
+            releasePlayer()
             initPlayer(url = streamUrl)
         }
-
-
     }
 
-    private val simpleCache: SimpleCache = AndroidApplication.simpleCache!!
+    override fun onStop() {
+        super.onStop()
+        releasePlayer()
+    }
 
     private fun initPlayer(url: String) {
-
         playerView = binding.playerView
 
         player = ExoPlayer.Builder(this)
-            .setMediaSourceFactory(PlayerUtil.getCacheMediaSourceFactory(simpleCache))
+            .setMediaSourceFactory(PlayerUtil.getCacheMediaSourceFactory(this, simpleCache))
             .build()
 
         playerView.player = player
 
-        player.setMediaSource(PlayerUtil.buildMediaSource(url, simpleCache))
+        player.setMediaSource(PlayerUtil.buildMediaSource(this, url, simpleCache))
 
         player.playWhenReady = true
 
         player.prepare()
     }
 
-    override fun onStop() {
-        super.onStop()
+    private fun releasePlayer() {
         if (::player.isInitialized) player.release()
     }
-
 
 }
--- app/src/main/java/com/example/baseandroidapp/util/PlayerUtil.kt
+++ app/src/main/java/com/example/baseandroidapp/util/PlayerUtil.kt
@@ -1,6 +1,8 @@
 package com.example.baseandroidapp.util
 
+import android.content.Context
 import android.net.Uri
+import com.example.baseandroidapp.R
 import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.source.DefaultMediaSourceFactory
@@ -19,36 +21,36 @@ import com.google.android.exoplayer2.util.Util
 object PlayerUtil {
 
     /** media source code */
-    fun buildMediaSource(url: String, cache: SimpleCache? = null): MediaSource {
+    fun buildMediaSource(context: Context, url: String, cache: SimpleCache? = null): MediaSource {
         val extension = url.split(".").last()
         val uri = Uri.parse(url)
 
         return if (cache == null) {
-            val dataSourceFactory = buildDataSourceFactory()
+            val dataSourceFactory = buildDataSourceFactory(context)
             createMediaSource(uri, extension, dataSourceFactory)
         } else {
-            val cacheDataSourceFactory = getCacheFactory(cache)
+            val cacheDataSourceFactory = getCacheFactory(context, cache)
             createMediaSource(uri, extension, cacheDataSourceFactory)
         }
     }
 
 
-    private fun getCacheFactory(simpleCache: SimpleCache) = CacheDataSource.Factory()
-        .setCache(simpleCache)
-        .setUpstreamDataSourceFactory(buildDataSourceFactory())
-        .setFlags(CacheDataSource.FLAG_IGNORE_CACHE_ON_ERROR)
+    private fun getCacheFactory(context: Context, simpleCache: SimpleCache) =
+        CacheDataSource.Factory()
+            .setCache(simpleCache)
+            .setUpstreamDataSourceFactory(buildDataSourceFactory(context))
+            .setFlags(CacheDataSource.FLAG_IGNORE_CACHE_ON_ERROR)
 
-    fun getCacheMediaSourceFactory(simpleCache: SimpleCache) = DefaultMediaSourceFactory(
-        getCacheFactory(simpleCache)
-    )
+    fun getCacheMediaSourceFactory(context: Context, simpleCache: SimpleCache) =
+        DefaultMediaSourceFactory(
+            getCacheFactory(context, simpleCache)
+        )
 
-    private fun buildDataSourceFactory(): DataSource.Factory {
-//        val userAgent = Util.getUserAgent(context, context.getString(R.string.app_name))
-        return buildHttpDataSourceFactory()
-    }
-
-    private fun buildHttpDataSourceFactory(): DataSource.Factory {
-        return DefaultHttpDataSource.Factory().setAllowCrossProtocolRedirects(true)
+    private fun buildDataSourceFactory(context: Context): DataSource.Factory {
+        val userAgent = Util.getUserAgent(context, context.getString(R.string.app_name))
+        return DefaultHttpDataSource.Factory()
+            .setAllowCrossProtocolRedirects(true)
+            .setUserAgent(userAgent)
     }
 
 
