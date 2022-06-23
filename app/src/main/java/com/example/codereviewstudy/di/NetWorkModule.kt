c70e06deb28b152b0ea82e39c130e4a9ac00026a
Jini <mjindev@gmail.com>
2022-06-23 15:20:31 +0900

    retrofit RxJava3CallAdapterFactory 추가

--- app/src/main/java/com/example/codereviewstudy/di/NetWorkModule.kt
+++ app/src/main/java/com/example/codereviewstudy/di/NetWorkModule.kt
@@ -12,6 +12,7 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.*
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import java.util.concurrent.TimeUnit
@@ -58,6 +59,7 @@ object NetworkModule {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(okHttpClient)
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .addConverterFactory(ScalarsConverterFactory.create())
             .build()
